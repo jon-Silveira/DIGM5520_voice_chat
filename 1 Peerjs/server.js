eb3933f6e627f69ca8552a5df340008f3fe2c396
js12lh <jonsilveirades@gmail.com>
2021-10-16 12:38:59 -0400

    Update server.js

--- 1 Peerjs/server.js
+++ 1 Peerjs/server.js
@@ -1,5 +1,36 @@
 //This code was found on this youtube video
 //https://www.youtube.com/watch?v=DvlyzDZDEq4&t
+/*
+//How To Create A Video Chat App With WebRTC - Web Dev Simplified
+https://www.youtube.com/watch?v=DvlyzDZDEq4&t
+Attempted fix and update - https://github.com/js12lh/DIGM5520_voice_chat/tree/main/1%20Peerjs
+
+This tutorial is one of the most popular youtube tutorials on creating video chat applications. 
+This tutorial was my first introduction to web and browser-based communications. In the tutorial, 
+the video chat application worked flawlessly each user can join a room through a browser, as they 
+join their video feed is added into the frame. Though in implementation there were plenty of issues.
+The application uses a socket.io server to exchange peer.js information. Peer.js is a wrapper for 
+WebRTC that simplifies the information exchange process. The information exchange allows for the 
+users to connect to each other’s browsers directly, an additional server is no longer necessary 
+after the connection is established. The benefit of this example is that the can add as many 
+individual clients as you need with performance loss. I was not able to test that capability
+due to several issues with the library, this was documented in the issue section of peerjs GitHub. 
+It appears that the last line in the library was incorrect and did not link to assisting pages properly.
+
+//# sourceMappingURL=/peerjs.min.js.map
+Several issues reported 
+https://github.com/peers/peerjs/issues/711
+https://github.com/peers/peerjs/issues/721
+answer found at https://github.com/peers/peerjs/issues/721
+
+Unfortunately, regardless of this fix, peerjs still does not work. The browser console does not error.
+Though there are moments when it connects after refreshing hundreds of times. I attempted to debug
+the application using socket.io the information exchange occurs but it appears that the peerjs
+library is the issue. Initially, I thought it was the configuration of peerjs STUN/ TURN servers 
+as there were multiple issues posted on GitHub of other STUN/TURN servers that were not working.
+https://gist.github.com/yetithefoot/7592580
+//
+*/
 
 const express = require('express')
 const app = express()
